0be9cd989cd0ccadf687b8e07a68aa1ed3ee2105
VladislavMagid <Vladislav.magid@gmail.com>
2024-07-30 19:24:57 +0200

    Lesson 18

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Homework15 from "homeworks/Homework15/Homework15"
 import Homework_16 from "homeworks/Homework_16/Homework_16"
 
 import Lesson_16 from "lessons/Lesson_16/Lesson_16"
+import Lesson_18 from "lessons/Lesson_18/Lesson_18"
 
 function App() {
   return (
     // <Homework15/>
     // <Lesson_16/>
-    <Homework_16 />
+    // <Homework_16 />
+    <Lesson_18></Lesson_18>
   )
 }
 
